5f6c582ad2950d07f684df210edf7a7d23418a90
James Halliday <substack@gmail.com>
2010-07-03 21:29:43 -0800

    new protocol-style for proxy example

--- examples/proxy.js
+++ examples/proxy.js
@@ -4,45 +4,33 @@ var sys = require('sys');
 var fs = require('fs');
 var http = require('http');
 
-DNode({
-    timesTen : function (n) { return n * 10 },
-}).listen(6060);
-
 var html = fs.readFileSync(__dirname + '/proxy.html');
-var js = {};
-try {
-    js['socket.io.js'] = fs.readFileSync(__dirname + '/socket.io.js');
-}
-catch (err) {
-    sys.puts(
-        'Symlink socket.io.js from Socket.IO into "'
-        + __dirname + '" to run this example'
-    );
-    process.exit();
-}
-js['dnode-client.js'] = fs.readFileSync(__dirname + '/../dnode-client.js');
-js['events.js'] = fs.readFileSync(__dirname + '/../events.js');
+var js = {
+    'dnode-client.js' : fs.readFileSync(__dirname + '/../dnode-client.js'),
+    'events.js' : fs.readFileSync(__dirname + '/../events.js'),
+    // Symlink socket.io.js to examples/ first
+    'socket.io.js' : fs.readFileSync(__dirname + '/socket.io.js'),
+};
 
 var httpServer = http.createServer(function (req,res) {
     var m = req.url.match(/^\/js\/(.+)/);
     if (m) {
-        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.writeHead(200, { 'Content-Type' : 'text/javascript' });
         res.end(js[m[1]]);
     }
     else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
         res.end(html);
     }
 });
 httpServer.listen(6061);
 
-DNode.SocketIO({
-    // no flashsocket in here on account of the annoying port 831 non-root issue
-    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
-}).proxy({
+// listen on 6060 and socket.io
+DNode({
+    timesTen : function (n) { return n * 10 },
+}).listen(6060).listen({
+    protocol : 'socket.io',
     server : httpServer,
-    nodes : {
-        times : 'localhost:6060'
-    },
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
 });
 
