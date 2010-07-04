b0b07b09aa593a36403cb485e2cdf26183223c31
James Halliday <substack@gmail.com>
2010-07-03 22:06:04 -0800

    far simpler way to do socket.io by wrapping around DNodeConn

--- dnode.js
+++ dnode.js
@@ -1,6 +1,7 @@
 var net = require('net');
 var sys = require('sys');
 var BufferList = require('bufferlist').BufferList;
+var Buffer = require('buffer').Buffer;
 var EventEmitter = require('events').EventEmitter;
 var io = require('socket.io');
 
@@ -46,7 +47,7 @@ function DNode (wrapper) {
             var port = firstType(arguments,'number') || kwargs.port;
             
             net.createServer(function (stream) {
-                var conn = new DNodeConn({
+                new DNodeConn({
                     stream : stream,
                     instance : f(),
                 });
@@ -58,24 +59,9 @@ function DNode (wrapper) {
             delete kwargs.server;
             delete kwargs.protocol;
             var socketIO = io.listen(server, kwargs);
-            
-            var sessions = {};
-            
-            socketIO.addListener('clientConnect', function (client) {
-                client.write(JSON.stringify({
-                    method : 'methods',
-                    arguments : [],
-                    id : -1,
-                }));
-            });
-            
-            socketIO.addListener('clientMessage', function (msgString,client) {
-                var msg = JSON.parse(msgString);
-                if (!(client.sessionId in sessions)) {
-                    sessions[session.id] =
-                        new DNode.SocketIO.Session(client, msg.result);
-                }
-                sessions[session.id].handleClient(msg);
+            DNodeSocketIO({
+                socketIO : socketIO,
+                wrapped : f,
             });
         }
         else {
@@ -101,21 +87,33 @@ function DNodeConn (args) {
     
     var bufferList = new BufferList;
     sock.addListener('data', function (buf) {
-        bufferList.push(buf);
-        var n = buf.toString().indexOf('\n');
-        while (n >= 0) {
-            var i = bufferList.length - (buf.length - n);
-            var line = bufferList.take(i); // up to the \n
-            bufferList.advance(i + 1); // past the \n
-            
-            var cmd = JSON.parse(line);
+        if (buf instanceof Buffer) {
+            bufferList.push(buf);
+            var n = buf.toString().indexOf('\n');
+            while (n >= 0) {
+                var i = bufferList.length - (buf.length - n);
+                var line = bufferList.take(i); // up to the \n
+                bufferList.advance(i + 1); // past the \n
+                
+                var cmd = JSON.parse(line);
+                if ('method' in cmd) {
+                    conn.emit('request', cmd);
+                }
+                else if ('result' in cmd) {
+                    conn.emit('response', cmd);
+                }
+                n = buf.toString().indexOf('\n', n + 1);
+            }
+        }
+        else if (typeof(buf) == 'string') {
+            // SocketIO wrapper sends strings
+            var cmd = JSON.parse(buf);
             if ('method' in cmd) {
                 conn.emit('request', cmd);
             }
             else if ('result' in cmd) {
                 conn.emit('response', cmd);
             }
-            n = buf.toString().indexOf('\n', n + 1);
         }
     });
     
@@ -185,6 +183,33 @@ function DNodeConn (args) {
     });
 };
 
+function DNodeSocketIO (params) {
+    var sock = params.socketIO;
+    
+    var streams = {};
+    StreamIO.prototype = new EventEmitter;
+    function StreamIO (client) {
+        this.write = function (msg) {
+            client.send(msg);
+        }
+    }
+    
+    sock.addListener('clientConnect', function (client) {
+        var id = client.sessionId;
+        streams[id] = new StreamIO(client);
+        new DNodeConn({
+            instance : params.wrapped(),
+            stream : streams[id],
+        });
+        streams[id].emit('connect');
+    });
+    
+    sock.addListener('clientMessage', function (msg,client) {
+        var id = client.sessionId;
+        streams[id].emit('data', msg);
+    });
+}
+
 exports.async = async;
 DNode.async = async;
 function async (f) {
@@ -192,106 +217,3 @@ function async (f) {
     return f;
 };
 
-DNode.SocketIO = {};
-DNode.SocketIO.Session = function Session (client, methods) {
-    this.id = client.sessionId;
-    
-    var reqId = 0;
-    var clientHandlers = {};
-    var objects = {}; // map nodes to client wrapped objects
-    var remotes = {}; // map nodes to dnode remote objects
-    var dnodes = {}; // map nodes to dnode connections
-    
-    function error (msg) {
-        client.write(JSON.stringify({
-            emit : 'error', arguments : [msg],
-        }));
-    }
-    
-    function requireKeys (msg, keys) {
-        var missing = keys.filter(function (key) {
-            return !(key in msg)
-        });
-        if (missing.length > 0) {
-            error('Missing keys ' + sys.inspect(missing)
-                + ' in request: ' + sys.inspect(msg));
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-    
-    // handle messages from the client
-    this.handleClient = function (msg) {
-        if ('result' in msg) {
-            // response from the client
-            if (requireKeys(msg, ['id','result'])) {
-                var f = handlers[msg.id];
-                f.call(objects[f.node], msg.result);
-            }
-        }
-        else if ('method' in msg) {
-            // request from the client
-            if (requireKeys(msg, ['node','method','arguments'])) {
-                var remote = remotes[msg.node];
-                remote[msg.method].apply(remote, msg.arguments);
-            }
-        }
-        else if ('connect' in msg) {
-            if (requireKeys(msg, ['node'])) {
-                this.connect(msg.node);
-            }
-        }
-        else if ('disconnect' in msg) {
-            if (requireKeys(msg, ['node'])) {
-                this.disconnect(msg.node);
-            }
-        }
-    };
-    
-    this.connect = function (node) {
-        if (!(node in nodes)) {
-            error('Node ' + sys.inspect(node) + ' is not available');
-        }
-        else {
-            // messages from each node come in through the object interface
-            var obj = {};
-            methods.forEach(function (method) {
-                obj[method] = DNode.async(function () {
-                    var args = [].concat.apply([],arguments);
-                    var argv = args.slice(0,-1);
-                    var f = args.slice(-1)[0];
-                    
-                    f.node = node;
-                    handlers[reqId] = f;
-                    client.write(JSON.stringify({
-                        method : method,
-                        arguments : argv,
-                        node : node,
-                        id : reqId ++,
-                    }));
-                    
-                });
-            });
-            objects[node] = obj;
-            
-            DNode(obj).connect(nodes[node], function (dnode, remote) {
-                remotes[node] = remote;
-                dnodes[node] = dnode;
-            });
-        }
-    };
-    
-    this.disconnect = function (node) {
-        if (node === undefined) {
-            // disconnect all connected nodes
-            Object.keys(dnodes).forEach(function (node) {
-                dnodes[node].end();
-            });
-        }
-        else {
-            dnodes[node].end();
-        }
-    };
-}
