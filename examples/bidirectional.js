f20906e25330ca984037f96247da700c1ced839e
James Halliday <substack@gmail.com>
2010-07-10 02:24:55 -0800

    bidirectional example now works

--- examples/bidirectional.js
+++ examples/bidirectional.js
@@ -4,16 +4,16 @@ var sys = require('sys');
 
 // server-side:
 DNode(function (client) {
-    this.timesX = DNode.async(function (n,f) {
+    this.timesX = function (n,f) {
         client.x(function (x) {
             f(n * x);
         });
-    }); 
+    }; 
 }).listen(6060);
 
 // client-side:
 DNode({
-    x : function () { return 20 }
+    x : function (f) { f(20) }
 }).connect(6060, function (remote) {
     remote.timesX(3, function (res) {
         sys.puts(res); // 20 * 3 == 60
