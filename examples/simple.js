a0bc80d958adfecaeac3c2a843fc7dea626bf2d1
James Halliday <substack@gmail.com>
2010-07-10 01:21:38 -0800

    fixed methods/remote bug but some other issues persist

--- examples/simple.js
+++ examples/simple.js
@@ -12,6 +12,7 @@ var server = DNode({
 // client-side:
 DNode.connect(6060, function (remote) {
     // note: this == remote
+    sys.puts(sys.inspect(remote));
     remote.moo(function (x) {
         sys.log(x);
     });
