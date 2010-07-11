d035bfd7fc3af00c7b52541f428168f7f61f4a34
Peteris Krumins <peteris.krumins@gmail.com>
2010-07-12 01:54:08 +0300

    updated nested example with nested remote calls and events

--- examples/nested.js
+++ examples/nested.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var DNode = require('dnode').DNode;
 var sys = require('sys');
+var EventEmitter = require('events').EventEmitter;
 
 // server-side:
 var server1 = DNode({
@@ -11,15 +12,38 @@ var server2 = DNode({
     timesTwenty : function (n,reply) { reply(n * 20) }
 }).listen(6061);
 
+var moo = new EventEmitter;
+
 // client-side:
 DNode.connect(6060, function (remote1) {
     DNode.connect(6061, function (remote2) {
-        remote2.timesTwenty(2, function (n) {
-            sys.puts(n); // 40
+        moo.addListener('hi', function (x) {
+            remote1.timesTen(x, function (res) {
+                sys.puts(res);
+                remote2.timesTwenty(res, function (res2) {
+                    sys.puts(res2);
+                });
+            });
         });
-        remote1.timesTen(5, function (n) {
-            sys.puts(n); // 50
+        remote2.timesTwenty(5, function (n) {
+            sys.puts(n); // 100
+            remote1.timesTen(0.1, function (n) {
+                sys.puts(n); // 1
+            });
         });
     });
 });
+
+setTimeout(function() {
+    moo.emit('hi', 100);
+}, 500);
+
+/*
+    Output:
+    100
+    1
+    ... after half a second ...
+    1000
+    20000
+*/
 
