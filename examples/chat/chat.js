6668537016bac84e66686b817503d9e21311b99a
James Halliday <substack@gmail.com>
2010-07-15 02:04:05 -0800

    awesome chat example

--- examples/chat/chat.js
+++ examples/chat/chat.js
@@ -3,12 +3,12 @@
 // Just as simple as plain socket.io since it's a simple problem.
 
 var DNode = require('dnode').DNode;
-var sys = require('sys');
 var fs = require('fs');
 var http = require('http');
 
-// load the html page and the client-side javascript into memory
+// load the files to serve up into memory
 var html = fs.readFileSync(__dirname + '/chat.html');
+var css = fs.readFileSync(__dirname + '/chat.css');
 var js = require('dnode/web').source();
 
 // simple http server to serve pages and for socket.io transport
@@ -16,6 +16,10 @@ var httpServer = http.createServer(function (req,res) {
     if (req.url == '/dnode.js') {
         res.writeHead(200, { 'Content-Type' : 'text/javascript' });
         res.end(js);
+    }
+    else if (req.url == '/chat.css') {
+        res.writeHead(200, { 'Content-Type' : 'text/css' });
+        res.end(css);
     }
     else {
         res.writeHead(200, { 'Content-Type' : 'text/html' });
@@ -25,15 +29,28 @@ var httpServer = http.createServer(function (req,res) {
 httpServer.listen(6061);
 
 // share the chat server routines with remote clients
-var clients = [];
-DNode(function (client) {
-    clients.push(client);
+function ChatServer (client, con) {
+    var name = '?';
+    
+    con.addListener('ready', function () {
+        client.name(function (who) {
+            con.broadcast('joined', who);
+            name = who;
+        });
+    });
+    
+    con.addListener('disconnect', function () {
+        con.broadcast('parted', name);
+    });
+    
     this.chat = function (who,msg) {
-        clients.forEach(function (c) { c.send(who,msg) });
+        con.broadcast('said', who, msg);
     };
-}).listen({
+}
+
+DNode(ChatServer).listen({
     protocol : 'socket.io',
     server : httpServer,
-    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'
+        .split(/\s+/),
 });
-
