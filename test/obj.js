796b5172249aef77d6d005fe22b75d7bd9642a24
James Halliday <substack@gmail.com>
2010-07-28 11:14:29 -0800

    object reference checks to make sure the reference doesn't get clobbered by scrubbing

---
+++ test/obj.js
@@ -0,0 +1,28 @@
+var DNode = require('dnode');
+
+exports['object ref tests'] = function (assert) {
+    var port = Math.floor(Math.random() * 40000 + 10000);
+    
+    var obj = { a : 1, b : 2, f : function (n,g) { g(n * 20) } };
+    
+    var server = DNode({
+        getObject : function (f) { f(obj) },
+    }).listen(port);
+    
+    DNode.connect(port, function (remote) {
+        remote.getObject(function (rObj) {
+            assert.equal(rObj.a, 1);
+            assert.equal(rObj.b, 2);
+            assert.equal(typeof rObj.f, 'function');
+            rObj.a += 100; rObj.b += 100;
+            assert.equal(obj.a, 1);
+            assert.equal(obj.b, 2);
+            assert.notEqual(obj.f, rObj.g);
+            assert.equal(typeof obj.f, 'function');
+            rObj.f(13, function (res) {
+                assert.equal(res, 260);
+                server.close();
+            });
+        });
+    });
+};
