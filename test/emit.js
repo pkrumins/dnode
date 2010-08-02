9b288b66ef0c5fe2e162644496f642358dfdb107
James Halliday <substack@gmail.com>
2010-08-01 19:38:17 -0800

    same function reference check in emit test, fails

--- test/emit.js
+++ test/emit.js
@@ -8,7 +8,7 @@ exports['event emitter test'] = function (assert) {
     
     DNode.expose(ev, 'emit');
     
-    ev.on('test', function (a, b, c) {
+    ev.on('test1', function (a, b, c) {
         assert.equal(a, 1);
         assert.equal(b, 2);
         assert.equal(sys.inspect(c), sys.inspect([3,4]));
@@ -18,7 +18,9 @@ exports['event emitter test'] = function (assert) {
     Server.prototype = new EventEmitter;
     function Server() {
         var self = this;
-        self.on = self.on;
+        DNode.expose(self, 'on');
+        DNode.expose(self, 'removeListener');
+        DNode.expose(self, 'emit');
         
         self.pass = function (name, em) {
             self.on(name, function () {
@@ -29,11 +31,12 @@ exports['event emitter test'] = function (assert) {
         };
         
         setTimeout(function () {
-            self.emit('test', 1, 2, [3,4]);
+            self.emit('test1', 1, 2, [3,4]);
+            self.emit('test2', 1337);
         }, 250);
         
         setTimeout(function () {
-            assert.ok(emitted, 'test event not emitted');
+            assert.ok(emitted, 'test1 event not emitted');
         }, 500);
     }
     
@@ -41,7 +44,14 @@ exports['event emitter test'] = function (assert) {
     
     var server = DNode(Server).listen(port);
     DNode.connect(port, function (remote) {
-        remote.pass('test', ev);
+        remote.pass('test1', ev);
+        var test2_calls = 0;
+        remote.on('test2', function f () {
+            test2_calls ++;
+            assert.ok(test2_calls == 1, 'test2 emitter not removed')
+            remote.removeListener('test2', f);
+            remote.emit('test2');
+        });
     });
     setTimeout(function () { server.end() }, 750);
 };
