10b5df05b27df5cbfb456da30bf1eb56d71ed7ef
James Halliday <substack@gmail.com>
2010-08-05 02:34:05 -0800

    less confusing bidirectional example

--- examples/bidirectional.js
+++ examples/bidirectional.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
 var DNode = require('dnode');
-var sys = require('sys');
 
 // server-side:
-DNode(function (client) {
-    this.timesX = function (n,f) {
-        client.x(function (x) {
-            f(n * x);
-        });
+var server = DNode(function (client) {
+    // Multiply the client's x times the supplied n.
+    // The result goes into the supplied callback cb.
+    this.xTimesN = function (n,cb) {
+        cb(client.x * n);
     }; 
 }).listen(6060);
 
 // client-side:
-DNode({
-    x : function (f) { f(20) }
-}).connect(6060, function (remote) {
-    remote.timesX(3, function (res) {
-        sys.puts(res); // 20 * 3 == 60
+DNode({ x : 20 }).connect(6060, function (remote) {
+    remote.xTimesN(3, function (result) {
+        console.log(result); // prints 60
+        server.end(); // kills the server
     });
 });
