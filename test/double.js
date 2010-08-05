2717cff9a9b54b8e69c07509ac4883cbf3130ba3
James Halliday <substack@gmail.com>
2010-08-05 00:16:29 -0800

    now another test that fails on account of the fix to the emitter test

---
+++ test/double.js
@@ -0,0 +1,35 @@
+var DNode = require('dnode');
+
+exports.double = function (assert) {
+    var port = Math.floor(Math.random() * 40000 + 10000);
+    
+    var server = DNode({
+        z : function (f, g, h) {
+            f(10, function (x) {
+                g(10, function (y) {
+                    h(x,y)
+                })
+            })
+        }
+    }).listen(port);
+    
+    DNode.connect(port, function (remote) {
+        remote.z(
+            function (x,f) { f(x * 2) },
+            function (x,f) { f(x / 2) },
+            function (x,y) {
+                assert.equal(x, 20, 'double, not equal');
+                assert.equal(y, 5, 'double, not equal');
+                server.end();
+            }
+        );
+        
+        function plusTen(n,f) { f(n + 10) }
+        
+        remote.z(plusTen, plusTen, function (x,y) {
+            assert.equal(x, 20, 'double, equal');
+            assert.equal(y, 20, 'double, equal');
+            server.end();
+        });
+    });
+};
