6526a4d9d15f026d18b65eb72c67d7cf5a268e39
James Halliday <substack@gmail.com>
2010-08-05 00:34:52 -0800

    new test for old undesired function wrappers for refs

---
+++ test/refs.js
@@ -0,0 +1,23 @@
+var DNode = require('dnode');
+var sys = require('sys');
+
+exports.refs = function (assert) {
+    var port = Math.floor(Math.random() * 40000 + 10000);
+    
+    var server = DNode({
+        a : 1,
+        b : 2,
+    }).listen(port);
+    
+    DNode.connect(port, function (remote) {
+        server.end();
+        assert.equal(
+            remote.a, 1,
+            'remote.a != 1, a = ' + sys.inspect(remote.a)
+        );
+        assert.equal(
+            remote.b, 1,
+            'remote.b != 1, a = ' + sys.inspect(remote.b)
+        );
+    });
+};
