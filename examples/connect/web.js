36352943af11fcb9a384af735cfa8026a300f170
James Halliday <substack@gmail.com>
2010-09-02 23:24:48 -0800

    traverse fix for socketio handling, connect example

---
+++ examples/connect/web.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+var DNode = require('dnode');
+var connect = require('connect');
+var http = require('http');
+
+var js = require('dnode/web').source();
+
+var server = connect.createServer(
+    connect.staticProvider(__dirname),
+    function (req, res) {
+        if (req.url == '/dnode.js') {
+            res.writeHead({ 'Content-Type' : 'text/javascript' });
+            res.end(js);
+        }
+    }
+).listen(4050);
+console.log('http://localhost:4050/');
+
+server.on('connection', function cb (socket) {
+    DNode(Server).listen(socket.server, {
+        transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/)
+    });
+    server.removeListener('connection', cb);
+});
+
+function Server (client) {
+    this.cat = function (cb) { cb('meow') };
+}
+
