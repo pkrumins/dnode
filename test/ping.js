44aca50dd6639bbf2024ae203d59af4ee26c202e
James Halliday <substack@gmail.com>
2010-09-14 01:14:16 -0800

    ping/timeout passes its test, bump to v0.3

--- test/ping.js
+++ test/ping.js
@@ -5,26 +5,40 @@ exports.ping = function (assert) {
     var port = Math.floor(Math.random() * 40000 + 10000);
     
     var server = DNode({
-        busy : function () { for (var i = 0; i < 1e8; i++); }
+        busy : function () { for (var i = 0; i < 1e7; i++); }
     }).listen(port);
     
     var pings = [];
+    var timeouts = [];
     
     server.on('ready', function () {
         DNode.connect(
-            port, { ping : 200, timeout : 50 },
+            port, { ping : 100, timeout : 50 },
             function (remote, conn) {
+                var iv = null;
                 setTimeout(function () {
-                    remote.busy();
-                }, 550);
+                    iv = setInterval(function () { remote.busy() }, 20);
+                }, 300);
                 
                 conn.on('ping', function (elapsed) {
                     pings.push(elapsed);
-                    if (pings.length == 5) server.end();
+                    
+                    if (pings.length == 6) {
+                        assert.ok(timeouts.length >= 2);
+                        
+                        pings.forEach(function (ms, i) {
+                            var t = timeouts.indexOf(i) >= 0;
+                            assert.equal(ms >= 50, t);
+                        });
+                        
+                        clearInterval(iv);
+                        server.end();
+                    }
                 });
                 
                 conn.on('timeout', function () {
-                    console.log('timeout!');
+                    assert.ok(pings.length >= 3);
+                    timeouts.push(pings.length);
                 });
             }
         );
