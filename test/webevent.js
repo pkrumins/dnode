a12c2c7f1b6cf295947e1445324c14d455853a90
James Halliday <substack@gmail.com>
2010-09-15 16:51:04 -0800

    webevent test actually loads the file

--- test/webevent.js
+++ test/webevent.js
@@ -1,11 +1,23 @@
+var fs = require('fs');
 var Script = process.binding('evals').Script;
 
 exports.webevent = function (assert) {
+    var file = __dirname + '/../lib/web/events.js';
+    
     Script.runInNewContext(
-        '(' + (function () {
-            assert.ok(true);
-        }).toString() + ')()',
-        { assert : assert },
-        __dirname + '/../lib/web/events.js'
+        fs.readFileSync(file) + '\n(' + emTest.toString() + ')()',
+        { assert : assert, setTimeout : setTimeout },
+        file
     );
 };
+
+function emTest () {
+    var em = new EventEmitter;
+    var mooed = false;
+    em.on('moo', function (x) {
+        assert.equal(x, 3);
+        mooed = true;
+    });
+    em.emit('moo', 3);
+    setTimeout(function () { assert.ok(mooed) }, 0);
+}
