4e277b230bc6781d00dbc1ae1e151d3d2e2914ca
James Halliday <substack@gmail.com>
2010-09-22 19:46:33 -0800

    multipart example to demonstrate kwargs port bug

---
+++ examples/multiport/web.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+var DNode = require('dnode');
+var sys = require('sys');
+var fs = require('fs');
+var http = require('http');
+
+// load the html page and the client-side javascript into memory
+var html = fs.readFileSync(__dirname + '/web.html');
+var js = require('dnode/web').source();
+
+// simple http server to serve pages and for socket.io transport
+var httpServer = http.createServer(function (req,res) {
+    if (req.url == '/dnode.js') {
+        res.writeHead(200, { 'Content-Type' : 'text/javascript' });
+        res.end(js);
+    }
+    else {
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(html);
+    }
+});
+httpServer.listen(6061);
+console.log('http://localhost:6061/');
+
+var sockServer = http.createServer();
+sockServer.listen(6062);
+
+// share an object with DNode over socket.io on top of the http server
+DNode(function (client) {
+    this.timesTen = function (n,f) { f(n * 10) };
+    this.whoAmI = function (reply) {
+        client.name(function (name) {
+            reply(name
+                .replace(/Mr\.?/,'Mister')
+                .replace(/Ms\.?/,'Miss')
+                .replace(/Mrs\.?/,'Misses')
+            );
+        })
+    };
+}).listen(sockServer, {
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
+});
+
