2aae5dfee6809d594e7ca8cecd9524cfa5b9753d
Joshua Holbrook <josh.holbrook@gmail.com>
2010-09-24 09:40:01 -0800

    Added a unit test for unicode.js.  Assertions do not catch error, but json.parse error output makes it obvious.

---
+++ test/unicode.js
@@ -0,0 +1,22 @@
+var DNode = require('dnode');
+
+exports.simple = function (assert) {
+    var port = Math.floor(Math.random() * 40000 + 10000);
+    
+    var server = DNode({
+        unicodes : function (reply) {
+            reply('☔☔☔☁☼☁❄');
+        }
+    }).listen(port);
+    
+    server.on('ready', function () {
+        DNode.connect(port, function (remote, conn) {
+            assert.equal(conn.stream.remoteAddress, '127.0.0.1');
+            assert.doesNotThrow( function () {
+                remote.unicodes(function (str) {
+                    assert.equal(str, '☔☔☔☁☼☁❄', 'remote unicodes == ☔☔☔☁☼☁❄');
+                });
+            });
+        });
+    });
+};
