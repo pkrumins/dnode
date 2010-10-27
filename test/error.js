3b1701552edf54cb7518c69ed70bb4a4282948a2
James Halliday <substack@gmail.com>
2010-10-27 04:58:02 +0000

    updated error test to catch local and remote error events

--- test/error.js
+++ test/error.js
@@ -18,10 +18,25 @@ exports.simple = function (assert) {
         };
     }).listen(port);
     
+    var ended = { client : false, server : false };
+    
+    var serverErrors = { local : [], remote : [] };
+    server.on('remoteError', function (err) {
+        serverErrors.remote.push(err);
+    });
+    server.on('localError', function (err) {
+        serverErrors.local.push(err);
+    });
+    server.on('end', function () {
+        assert.equal(serverErrors.remote.length, 1);
+        assert.equal(serverErrors.local.length, 2);
+        ended.server = true;
+    });
+    
     var errNum = 0;
     var caughtLocal = false;
     server.on('ready', function () {
-        DNode(function (client, conn) {
+        var client = DNode(function (client, conn) {
             conn.on('remoteError', function (err) {
                 errNum ++;
                 if (errNum == 1) {
@@ -42,6 +57,12 @@ exports.simple = function (assert) {
                 caughtLocal = true;
             });
             
+            conn.on('end', function () {
+                assert.equal(clientErrors.remote.length, 2);
+                assert.equal(clientErrors.local.length, 1);
+                ended.client = true;
+            });
+                
             this.pow = function () {
                 throw 'Local error';
             };
@@ -50,11 +71,23 @@ exports.simple = function (assert) {
             remote.two();
             remote.three();
         });
+        
+        var clientErrors = { local : [], remote : [] };
+        client.on('localError', function (err) {
+            clientErrors.local.push(err);
+        });
+        client.on('remoteError', function (err) {
+            clientErrors.remote.push(err);
+        });
     });
     
     setTimeout(function () {
         assert.equal(errNum, 2);
         assert.ok(caughtLocal);
         server.end();
+        setTimeout(function () {
+            assert.ok(ended.server);
+            assert.ok(ended.client);
+        }, 50);
     }, 200);
 };
