56e33928dba54186a0d9aaac530e479fe773c756
James Halliday <substack@gmail.com>
2010-11-08 21:41:44 +0000

    default web route seems to work

--- examples/connect/web.js
+++ examples/connect/web.js
@@ -6,13 +6,14 @@ var http = require('http');
 var js = require('dnode/web').source();
 
 var server = connect.createServer(
-    connect.staticProvider(__dirname),
-    require('dnode/web').route('/dnode.js')
+    connect.staticProvider(__dirname)
 ).listen(4050);
 console.log('http://localhost:4050/');
 
 DNode(Server).listen(server, {
-    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/)
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'.split(/\s+/),
+    // if you want to be explicit:
+    // route : '/dnode.js'
 });
 
 function Server (client) {
