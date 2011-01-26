eca9f20119ca6b1a1c3f408e253659913050e350
James Halliday <substack@gmail.com>
2011-01-26 07:14:24 +0000

    synchronous middleware but that should be good enough

--- test/middleware.js
+++ test/middleware.js
@@ -3,7 +3,6 @@ var assert = require('assert');
 
 exports.middleware = function () {
     var port = Math.floor(Math.random() * 40000 + 10000);
-    /*
     var tf = setTimeout(function () {
         assert.fail('never finished');
     }, 1000);
@@ -11,38 +10,38 @@ exports.middleware = function () {
     var tr = setTimeout(function () {
         assert.fail('never ready');
     }, 1000);
-    */
+    
+    var tc = setTimeout(function () {
+        assert.fail('connection not ready');
+    }, 1000);
     
     var server = dnode(function (client, conn) {
-        if (!conn.zing) {
-            server.close();
-            conn.end();
-        }
-        assert.ok(conn.zing);
+        assert.ok(!conn.zing);
         assert.ok(!client.moo);
-        conn.on('ready', function () {
-            //clearTimeout(tr);
-            assert.ok(client.moo);
-        });
+        
+        conn.on('ready', (function () {
+            clearTimeout(tr);
+            assert.ok(conn.zing);
+            assert.ok(this.moo);
+        }).bind(this));
+        
         this.baz = 42;
     }).listen(port);
     
-    server.use(function (client, conn, next) {
-        setTimeout(function () {
-            conn.zing = true;
-            next();
-        }, 100);
+    server.use(function (client, conn) {
+        conn.zing = true;
     });
     
-    server.use(function (client, conn, next) {
+    server.use(function (client, conn) {
+        this.moo = true;
         conn.on('ready', function () {
-            client.moo = true;
+            clearTimeout(tc);
         });
     });
     
     server.on('ready', function () {
         dnode.connect(port, function (remote, conn) {
-            //clearTimeout(tf);
+            clearTimeout(tf);
             assert.ok(remote.baz);
             conn.end();
             server.close();
