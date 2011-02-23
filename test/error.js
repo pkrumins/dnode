a3aea48d663b7c2b86bb86d3ca384b83c7a8850f
James Halliday <substack@gmail.com>
2011-02-22 21:04:01 -0800

    error test passes now

--- test/error.js
+++ test/error.js
@@ -1,10 +1,15 @@
 var DNode = require('dnode');
 var sys = require('sys');
 
-exports.simple = function (assert) {
+exports.errors = function (assert) {
     var port = Math.floor(Math.random() * 40000 + 10000);
+    var errors = { server : [], client : [] };
     
-    var server = DNode(function (remote) {
+    var server = DNode(function (remote, conn) {
+        conn.on('error', function (err) {
+            errors.server.push(err);
+        });
+        
         this.one = function () {
             throw 'string throw'
         };
@@ -18,51 +23,31 @@ exports.simple = function (assert) {
         };
     }).listen(port);
     
-    var ended = { client : false, server : false };
+    var ts = setTimeout(function () {
+        assert.fail('server never ended');
+    }, 5000);
     
-    var serverErrors = { local : [], remote : [] };
-    server.on('remoteError', function (err) {
-        serverErrors.remote.push(err);
-    });
-    server.on('localError', function (err) {
-        serverErrors.local.push(err);
-    });
     server.on('end', function () {
-        assert.equal(serverErrors.remote.length, 1);
-        assert.equal(serverErrors.local.length, 2);
-        ended.server = true;
+        clearTimeout(ts);
+        assert.eql(errors.server[0], 'string throw');
+        
+        try { undefined.name }
+        catch (refErr) {
+            assert.eql(refErr.message, errors.server[1].message);
+            assert.eql(refErr.type, errors.server[1].type);
+        }
+        
+        assert.equal(errors.server.length, 2);
+        
+        assert.eql(errors.client, [ 'Local error' ]);
     });
     
-    var errNum = 0;
-    var caughtLocal = false;
     server.on('ready', function () {
         var client = DNode(function (client, conn) {
-            conn.on('remoteError', function (err) {
-                errNum ++;
-                if (errNum == 1) {
-                    assert.equal(err, 'string throw');
-                }
-                else if (errNum == 2) {
-                    try { undefined.name }
-                    catch (refErr) {
-                        assert.equal(err.name, refErr.name);
-                        assert.equal(err.message, refErr.message);
-                        assert.equal(err.type, refErr.type);
-                    }
-                }
+            conn.on('error', function (err) {
+                errors.client.push(err);
             });
             
-            conn.on('localError', function (err) {
-                assert.equal(err, 'Local error');
-                caughtLocal = true;
-            });
-            
-            conn.on('end', function () {
-                assert.equal(clientErrors.remote.length, 2);
-                assert.equal(clientErrors.local.length, 1);
-                ended.client = true;
-            });
-                
             this.pow = function () {
                 throw 'Local error';
             };
@@ -70,24 +55,9 @@ exports.simple = function (assert) {
             remote.one();
             remote.two();
             remote.three();
-        });
-        
-        var clientErrors = { local : [], remote : [] };
-        client.on('localError', function (err) {
-            clientErrors.local.push(err);
-        });
-        client.on('remoteError', function (err) {
-            clientErrors.remote.push(err);
+            setTimeout(function () {
+                server.end();
+            }, 100);
         });
     });
-    
-    setTimeout(function () {
-        assert.equal(errNum, 2);
-        assert.ok(caughtLocal);
-        server.end();
-        setTimeout(function () {
-            assert.ok(ended.server);
-            assert.ok(ended.client);
-        }, 50);
-    }, 200);
 };
