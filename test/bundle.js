9cf2087cc46347a4296eb9da3b6bc95876d1327e
James Halliday <mail@substack.net>
2011-06-03 14:50:22 -0700

    express test that passes already hmmm

--- test/bundle.js
+++ test/bundle.js
@@ -1,8 +1,9 @@
 var assert = require('assert');
 var dnode = require('dnode');
 var http = require('http');
+var express = require('express');
 
-exports.checkCookie = function () {
+exports.checkCookieHTTP = function () {
     var port = Math.floor(10000 + (Math.random() * Math.pow(2,16) - 10000));
     
     var web = http.createServer(function (req, res) {
@@ -23,10 +24,45 @@ exports.checkCookie = function () {
             path : '/dnode.js',
         };
         http.get(req, function (res) {
+            assert.equal(res.statusCode, 200);
             assert.equal(res.headers['set-cookie'], 'foo=bar');
             assert.equal(res.headers['content-type'], 'text/javascript');
             
             web.close();
+            server.end();
+        });
+    });
+};
+
+exports.checkCookieExpress = function () {
+    var port = Math.floor(10000 + (Math.random() * Math.pow(2,16) - 10000));
+    
+    var app = express.createServer();
+    app.use(function (req, res, next) {
+        res.setHeader('set-cookie', [ 'foo=bar' ]);
+        next();
+    }); 
+    
+    app.get('/', function (req, res) {
+        res.setStatus(200);
+        res.setHeader('content-type', 'text/html');
+        res.end('pow');
+    });
+    
+    var server = dnode().listen(app, { io : { log : null } });
+    
+    app.listen(port, function () {
+        var req = {
+            host : 'localhost',
+            port : port,
+            path : '/dnode.js',
+        };
+        http.get(req, function (res) {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers['set-cookie'], 'foo=bar');
+            assert.equal(res.headers['content-type'], 'text/javascript');
+            
+            app.close();
             server.end();
         });
     });
