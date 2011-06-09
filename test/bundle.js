182fa5acfb1bac938f924cd320a23571836801f2
James Halliday <mail@substack.net>
2011-06-08 20:27:18 -0700

    turn cookie http test into just http

--- test/bundle.js
+++ test/bundle.js
@@ -3,12 +3,10 @@ var dnode = require('dnode');
 var http = require('http');
 var express = require('express');
 
-exports.checkCookieHTTP = function () {
+exports.checkHTTP = function () {
     var port = Math.floor(10000 + (Math.random() * Math.pow(2,16) - 10000));
     
     var web = http.createServer(function (req, res) {
-        res.setHeader('set-cookie', [ 'foo=bar' ]);
-        
         if (req.url === '/') {
             res.setStatus(200);
             res.setHeader('content-type', 'text/html');
@@ -25,7 +23,6 @@ exports.checkCookieHTTP = function () {
         };
         http.get(req, function (res) {
             assert.equal(res.statusCode, 200);
-            assert.equal(res.headers['set-cookie'], 'foo=bar');
             assert.equal(res.headers['content-type'], 'text/javascript');
             
             web.close();
