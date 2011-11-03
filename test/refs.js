3cea9d800f64695777cac92226ac14b54cca9045
James Halliday <mail@substack.net>
2011-11-03 15:15:55 -0700

    refs test using tap

--- test/refs.js
+++ test/refs.js
@@ -1,7 +1,8 @@
 var dnode = require('../');
-var assert = require('assert');
+var test = require('tap').test;
 
-exports.refs = function () {
+test('refs', function (t) {
+    t.plan(2);
     var port = Math.floor(Math.random() * 40000 + 10000);
     
     var server = dnode({
@@ -13,8 +14,9 @@ exports.refs = function () {
         dnode.connect(port, function (remote, conn) {
             conn.end();
             server.close();
-            assert.equal(remote.a, 1);
-            assert.equal(remote.b, 2);
+            t.equal(remote.a, 1);
+            t.equal(remote.b, 2);
+            t.end();
         });
     });
-};
+});
