a76eab61ee91d7f0c359f99f5d4b8fa6ca4403ff
James Halliday <mail@substack.net>
2011-11-03 15:10:53 -0700

    _id test using tap

--- test/_id.js
+++ test/_id.js
@@ -1,16 +1,18 @@
 var dnode = require('../');
-var assert = require('assert');
+var test = require('tap').test;
 
-exports._id = function () {
+test('_id', function (t) {
+    t.plan(1);
     var port = Math.floor(Math.random() * 40000 + 10000);
     
     var server = dnode({ _id : 1337 }).listen(port);
     
     server.on('ready', function () {
         dnode.connect(port, function (remote, conn) {
-            assert.eql(remote._id, 1337);
+            t.equal(remote._id, 1337);
             conn.end();
             server.close();
+            t.end();
         });
     });
-};
+});
