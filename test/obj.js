9a5ca9c13c04120b497e2f665fd76b648623dd7c
James Halliday <mail@substack.net>
2011-11-03 15:14:14 -0700

    obj test using tap

--- test/obj.js
+++ test/obj.js
@@ -1,7 +1,8 @@
 var dnode = require('../');
-var assert = require('assert');
+var test = require('tap').test;
 
-exports['object ref tests'] = function () {
+test('object ref tests', function (t) {
+    t.plan(8);
     var port = Math.floor(Math.random() * 40000 + 10000);
     
     var obj = { a : 1, b : 2, f : function (n,g) { g(n * 20) } };
@@ -13,20 +14,21 @@ exports['object ref tests'] = function () {
     server.on('ready', function () {
         dnode.connect(port, function (remote, conn) {
             remote.getObject(function (rObj) {
-                assert.equal(rObj.a, 1);
-                assert.equal(rObj.b, 2);
-                assert.equal(typeof rObj.f, 'function');
+                t.equal(rObj.a, 1);
+                t.equal(rObj.b, 2);
+                t.equal(typeof rObj.f, 'function');
                 rObj.a += 100; rObj.b += 100;
-                assert.equal(obj.a, 1);
-                assert.equal(obj.b, 2);
-                assert.notEqual(obj.f, rObj.g);
-                assert.equal(typeof obj.f, 'function');
+                t.equal(obj.a, 1);
+                t.equal(obj.b, 2);
+                t.notEqual(obj.f, rObj.g);
+                t.equal(typeof obj.f, 'function');
                 rObj.f(13, function (res) {
-                    assert.equal(res, 260);
+                    t.equal(res, 260);
                     conn.end();
                     server.close();
+                    t.end();
                 });
             });
         });
     });
-};
+});
